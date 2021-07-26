9892f15bfe09dbd6044a9059a7cc3032ff477357
AmirJala <Amir.Sadour@jalasoft.com>
2021-07-26 12:54:42 -0500

    Creating items basic CRUD

---
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Service.Dtos;
+
+namespace Play.Catalog.Service.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> items = new()
+        {
+            new ItemDto(Guid.NewGuid(), "Potion", "Restore a small amount of HP", 5, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Bronze Sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow)
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ItemDto GetById(Guid id)
+        {
+            return items.Find( item => item.Id == id);
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description,
+                        createItemDto.Price, DateTimeOffset.UtcNow);
+            return CreatedAtAction(nameof(GetById), new {id = item.Id}, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            ItemDto existingItem = GetById(id);
+            if(existingItem != null)
+            {
+                var updatedItem = existingItem with
+                {
+                    Name = updateItemDto.Name,
+                    Description = updateItemDto.Description,
+                    Price = updateItemDto.Price
+                };
+
+                var index = items.FindIndex(item => item.Id == id);
+                items[index] = updatedItem;
+
+                return Ok();
+            }
+
+            return NotFound("Couldn't be find an item with that id");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var index = items.FindIndex(item => item.Id == id);  
+            if(index != -1)
+            {
+                items.RemoveAt(index);
+                return Ok();
+            }     
+
+            return NotFound("Couldn't be find an item with that id");
+        }
+    }
+}
